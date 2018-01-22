56716202bab00093ed76808cf029da24cef747bf
Fabiano Galvão de Carvalho <carvalho.fabiano@gmail.com>
2018-01-22 17:24:09 -0200

    Alguns ajustes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="content">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="content-header">
             <div class="panel panel-default" >
                 <div class="panel-heading">Seja bem vindo <b>{{Auth::user()->name}}</b>!!!</div>
+                    <div class="clearfix"></div>
+                        <div class="content">
+                            <div class="panel-body">
+                                @if(!empty ( $convivencia ))
+                                <font color="red">
+                                <b>* <u>Próxima Convivência:</u> {!! $convivencia->no_nome !!}</b><br>
+                                <b>* <u>Data:</u> {!! Carbon\Carbon::parse($convivencia->dt_inicio)->format('d/m/Y') !!} a {!! Carbon\Carbon::parse($convivencia->dt_fim)->format('d/m/Y') !!}<br></b>
+                                <b>* <u>Fim das inscrições:</u> {!! Carbon\Carbon::parse($convivencia->dt_fim_inscricao)->format('d/m/Y') !!}</b><br>
+                                <b>* <u>Local:</u> {!! $local->where('id', $convivencia->local_convivencia_id)->pluck('no_local')->first() !!}</b><br>                    
+                                <a class="btn btn-primary pull-left" style="margin-top: 25px" href="{!! route('convivencias.lista_ativas') !!}">Inscrição para Convivência</a>
+                                <br><br><br><br>
+                                </font>
+                                @else
+                                <h2><font color="red">Não existem convivências com inscrições abertas</font></h2><br><br>
+                                @endif
 
+                                Aqui colocaremos um quadro de avisos gerais e informações sobre as convivências que estão com as inscrições abertas e um link para inscrição em cada uma.
 
-                <div class="panel-body">
-                    @if(!empty ( $convivencia ))
-                    <font color="red">
-                    <br>
-                    <b>* <u>Próxima Convivência:</u> {!! $convivencia->no_nome !!}</b><br>
-                    <b>* <u>Data:</u> {!! Carbon\Carbon::parse($convivencia->dt_inicio)->format('d/m/Y') !!} a {!! Carbon\Carbon::parse($convivencia->dt_fim)->format('d/m/Y') !!}<br></b>
-                    <b>* <u>Fim das inscrições:</u> {!! Carbon\Carbon::parse($convivencia->dt_fim_inscricao)->format('d/m/Y') !!}</b><br>
-                    <b>* <u>Local:</u> {!! $local->where('id', $convivencia->local_convivencia_id)->pluck('no_local')->first() !!}</b><br>                    
-                    <a class="btn btn-primary pull-left" style="margin-top: 25px" href="{!! route('convivencias.lista_ativas') !!}">Inscrição para Convivência</a>
-                    <br><br><br><br>
-                    </font>
-                    @else
-                    <h2><font color="red">Não existem convivências com inscrições abertas</font></h2><br><br>
-                    @endif
+                                <br><br>
 
-                    Aqui colocaremos um quadro de avisos gerais e informações sobre as convivências que estão com as inscrições abertas e um link para inscrição em cada uma.
+                                Por enquanto, acesse o MENU ao lado para mais opções...<br><br>
 
-                    <br><br>
-
-                    Por enquanto, acesse o MENU ao lado para mais opções...<br><br>
-
-                    O responsável começa cadastrando os membros da sua Equipe e depois deve inscrevê-los ou não na Convivência.<br><br>
-                    
-                    Os MENUS de ADMINISTRAÇÃO serão acessados somente pelos Administradores do Sistema para criação de NOVAS CONVIVÊNCIAS e inclusão de novos Tipos de Quarto
+                                O responsável começa cadastrando os membros da sua Equipe e depois deve inscrevê-los ou não na Convivência.<br><br>
+                                
+                                Os MENUS de ADMINISTRAÇÃO e TABELAS DE APOIO serão acessados somente pelos Administradores do Sistema para criação de NOVAS CONVIVÊNCIAS, MANUTENÇÃO DE USUÁRIOS, PERMISSÕES e inclusão de novos Tipos de Quarto, translados, etc.
+                            </div>
                 </div>
             </div>
         </div>
--- storage/framework/views/8208bf065eee88c210bbec59a0c4433a8968f6a7.php
+++ storage/framework/views/8208bf065eee88c210bbec59a0c4433a8968f6a7.php
@@ -1,35 +1,35 @@
 <?php $__env->startSection('content'); ?>
-<div class="container">
+<div class="content">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="content-header">
             <div class="panel panel-default" >
                 <div class="panel-heading">Seja bem vindo <b><?php echo e(Auth::user()->name); ?></b>!!!</div>
+                    <div class="clearfix"></div>
+                        <div class="content">
+                            <div class="panel-body">
+                                <?php if(!empty ( $convivencia )): ?>
+                                <font color="red">
+                                <b>* <u>Próxima Convivência:</u> <?php echo $convivencia->no_nome; ?></b><br>
+                                <b>* <u>Data:</u> <?php echo Carbon\Carbon::parse($convivencia->dt_inicio)->format('d/m/Y'); ?> a <?php echo Carbon\Carbon::parse($convivencia->dt_fim)->format('d/m/Y'); ?><br></b>
+                                <b>* <u>Fim das inscrições:</u> <?php echo Carbon\Carbon::parse($convivencia->dt_fim_inscricao)->format('d/m/Y'); ?></b><br>
+                                <b>* <u>Local:</u> <?php echo $local->where('id', $convivencia->local_convivencia_id)->pluck('no_local')->first(); ?></b><br>                    
+                                <a class="btn btn-primary pull-left" style="margin-top: 25px" href="<?php echo route('convivencias.lista_ativas'); ?>">Inscrição para Convivência</a>
+                                <br><br><br><br>
+                                </font>
+                                <?php else: ?>
+                                <h2><font color="red">Não existem convivências com inscrições abertas</font></h2><br><br>
+                                <?php endif; ?>
 
+                                Aqui colocaremos um quadro de avisos gerais e informações sobre as convivências que estão com as inscrições abertas e um link para inscrição em cada uma.
 
-                <div class="panel-body">
-                    <?php if(!empty ( $convivencia )): ?>
-                    <font color="red">
-                    <br>
-                    <b>* <u>Próxima Convivência:</u> <?php echo $convivencia->no_nome; ?></b><br>
-                    <b>* <u>Data:</u> <?php echo Carbon\Carbon::parse($convivencia->dt_inicio)->format('d/m/Y'); ?> a <?php echo Carbon\Carbon::parse($convivencia->dt_fim)->format('d/m/Y'); ?><br></b>
-                    <b>* <u>Fim das inscrições:</u> <?php echo Carbon\Carbon::parse($convivencia->dt_fim_inscricao)->format('d/m/Y'); ?></b><br>
-                    <b>* <u>Local:</u> <?php echo $local->where('id', $convivencia->local_convivencia_id)->pluck('no_local')->first(); ?></b><br>                    
-                    <a class="btn btn-primary pull-left" style="margin-top: 25px" href="<?php echo route('convivencias.lista_ativas'); ?>">Inscrição para Convivência</a>
-                    <br><br><br><br>
-                    </font>
-                    <?php else: ?>
-                    <h2><font color="red">Não existem convivências com inscrições abertas</font></h2><br><br>
-                    <?php endif; ?>
+                                <br><br>
 
-                    Aqui colocaremos um quadro de avisos gerais e informações sobre as convivências que estão com as inscrições abertas e um link para inscrição em cada uma.
+                                Por enquanto, acesse o MENU ao lado para mais opções...<br><br>
 
-                    <br><br>
-
-                    Por enquanto, acesse o MENU ao lado para mais opções...<br><br>
-
-                    O responsável começa cadastrando os membros da sua Equipe e depois deve inscrevê-los ou não na Convivência.<br><br>
-                    
-                    Os MENUS de ADMINISTRAÇÃO serão acessados somente pelos Administradores do Sistema para criação de NOVAS CONVIVÊNCIAS e inclusão de novos Tipos de Quarto
+                                O responsável começa cadastrando os membros da sua Equipe e depois deve inscrevê-los ou não na Convivência.<br><br>
+                                
+                                Os MENUS de ADMINISTRAÇÃO e TABELAS DE APOIO serão acessados somente pelos Administradores do Sistema para criação de NOVAS CONVIVÊNCIAS, MANUTENÇÃO DE USUÁRIOS, PERMISSÕES e inclusão de novos Tipos de Quarto, translados, etc.
+                            </div>
                 </div>
             </div>
         </div>
