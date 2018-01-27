4ac613e6c6536e29734d3fb0bcfe5c5c2898f523
Fabiano Galvão <carvalho.fabiano@gmail.com>
2018-01-27 21:05:50 -0200

    Ajuste jQuery combobox

--- resources/views/convivencias/lista_ativasfields.blade.php
+++ resources/views/convivencias/lista_ativasfields.blade.php
@@ -1,14 +1,11 @@
 
-<div class="form-group col-sm-6">
 @foreach($convivencias as $convivencia)
 
 	@isset ($convivencia->is_ativo)
-	<div class="form-group col-sm-6">
+	<div class="form-group col-sm-1">
 	    {!! Form::label('convivencia_id', 'Convivência') !!}
 	    {!! Form::select('convivencia_id', $convivencias->pluck('no_nome', 'id'), ['id' => 'convivencia_id', 'class' => 'form-control', 'dropdown-menu'])!!}
 	</div>
-
-
 <!-- Submit Field -->
 	<div class="form-group col-sm-12">
     	{!! Form::submit('Inscrever Equipe' , ['class' => 'btn btn-primary']) !!}
@@ -24,6 +21,6 @@
 	   	<a href="{!! route('membros.index') !!}" class="btn btn-primary pull-left" style="margin-top: 25px">Voltar para lista de Membros</a>
 	</div>
 @endempty
-</div>
+
 
 
--- storage/framework/views/576f2ef9004ae9643413d9e29e88f5aea373d413.php
+++ storage/framework/views/576f2ef9004ae9643413d9e29e88f5aea373d413.php
@@ -1,16 +1,13 @@
 
-<div class="form-group col-sm-6">
 <?php $__currentLoopData = $convivencias; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $convivencia): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 
 	<?php if(isset($convivencia->is_ativo)): ?>
-	<div class="form-group col-sm-6">
+	<div class="form-group col-sm-1">
 	    <?php echo Form::label('convivencia_id', 'Convivência'); ?>
 
-	    <?php echo Form::combobox('convivencia_id', $convivencias->pluck('no_nome', 'id'), ['id' => 'convivencia_id', 'class' => 'form-control', 'dropdown-menu']); ?>
+	    <?php echo Form::select('convivencia_id', $convivencias->pluck('no_nome', 'id'), ['id' => 'convivencia_id', 'class' => 'form-control', 'dropdown-menu']); ?>
 
 	</div>
-
-
 <!-- Submit Field -->
 	<div class="form-group col-sm-12">
     	<?php echo Form::submit('Inscrever Equipe' , ['class' => 'btn btn-primary']); ?>
@@ -27,6 +24,6 @@
 	   	<a href="<?php echo route('membros.index'); ?>" class="btn btn-primary pull-left" style="margin-top: 25px">Voltar para lista de Membros</a>
 	</div>
 <?php endif; ?>
-</div>
+
 
 
